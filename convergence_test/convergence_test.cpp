8381971d3fc737851aa3b78471cdd5c6d7e699f1
ds283 <D.Seery@sussex.ac.uk>
2016-06-26 22:07:10 +0100

    Improve job for convergence test
    - make use of new platform functionality to support CSV outputs
    
    - and labels and tags to each derived line, which helps identify columns in the final table
    
    - fix some typos in the SQL queries
    
    - add table containing timing data
    
    - change convergence table to use wavenumber series rather than time series. This means less postprocessing is required to construct plots; basically we can transpose the wavenumber-series table to get the right set of columns, whereas post-processing the time-series table is much more work

--- convergence_test/convergence_test.cpp
+++ convergence_test/convergence_test.cpp
@@ -70,59 +70,133 @@ void write_tasks(transport::repository<>& repo, transport::dquad_mpi<>* model)
     transport::zeta_threepf_task<> ztk750 = make_task(repo, ics, N_init, N_end, 7.50);
     transport::zeta_threepf_task<> ztk775 = make_task(repo, ics, N_init, N_end, 7.75);
     transport::zeta_threepf_task<> ztk800 = make_task(repo, ics, N_init, N_end, 8.00);
-    transport::zeta_threepf_task<> ztk825 = make_task(repo, ics, N_init, N_end, 8.25);
-    transport::zeta_threepf_task<> ztk850 = make_task(repo, ics, N_init, N_end, 8.50);
-    transport::zeta_threepf_task<> ztk875 = make_task(repo, ics, N_init, N_end, 8.75);
-    transport::zeta_threepf_task<> ztk900 = make_task(repo, ics, N_init, N_end, 9.00);
-    transport::zeta_threepf_task<> ztk925 = make_task(repo, ics, N_init, N_end, 9.25);
-    transport::zeta_threepf_task<> ztk950 = make_task(repo, ics, N_init, N_end, 9.50);
-    transport::zeta_threepf_task<> ztk975 = make_task(repo, ics, N_init, N_end, 9.75);
-    transport::zeta_threepf_task<> ztk1000 = make_task(repo, ics, N_init, N_end, 10.00);
 
-    vis_toolkit::SQL_time_query last_time("tserial IN (SELECT MAX(teserial) FROM time_samples)");
+    vis_toolkit::SQL_time_query last_time("serial IN (SELECT MAX(serial) FROM time_samples)");
     vis_toolkit::SQL_threepf_query all_threepfs("1=1");
 
-    vis_toolkit::zeta_threepf_time_series<> z300(ztk300, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z325(ztk325, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z350(ztk350, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z375(ztk375, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z400(ztk400, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z425(ztk425, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z450(ztk450, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z475(ztk475, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z500(ztk500, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z525(ztk525, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z550(ztk550, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z575(ztk575, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z600(ztk600, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z625(ztk625, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z650(ztk650, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z675(ztk675, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z700(ztk700, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z725(ztk725, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z750(ztk750, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z775(ztk775, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z800(ztk800, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z825(ztk825, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z850(ztk850, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z875(ztk875, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z900(ztk900, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z925(ztk925, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z950(ztk950, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z975(ztk975, last_time, all_threepfs);
-    vis_toolkit::zeta_threepf_time_series<> z1000(ztk1000, last_time, all_threepfs);
+    vis_toolkit::zeta_threepf_wavenumber_series<> z300(ztk300, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost300(*dynamic_cast< transport::threepf_task<>* >(ztk300.get_parent_task()), all_threepfs);
+    z300.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("300", "300");
+    cost300.set_label_text("300", "300");
 
-    vis_toolkit::time_series_table<> table("table", "table.txt");
-    table += z300 + z325 + z350 + z375
+    vis_toolkit::zeta_threepf_wavenumber_series<> z325(ztk325, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost325(*dynamic_cast< transport::threepf_task<>* >(ztk325.get_parent_task()), all_threepfs);
+    z325.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("325", "325");
+    cost325.set_label_text("325", "325");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z350(ztk350, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost350(*dynamic_cast< transport::threepf_task<>* >(ztk350.get_parent_task()), all_threepfs);
+    z350.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("350", "350");
+    cost350.set_label_text("350", "350");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z375(ztk375, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost375(*dynamic_cast< transport::threepf_task<>* >(ztk375.get_parent_task()), all_threepfs);
+    z375.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("375", "375");
+    cost375.set_label_text("375", "375");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z400(ztk400, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost400(*dynamic_cast< transport::threepf_task<>* >(ztk400.get_parent_task()), all_threepfs);
+    z400.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("400", "400");
+    cost400.set_label_text("400", "400");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z425(ztk425, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost425(*dynamic_cast< transport::threepf_task<>* >(ztk425.get_parent_task()), all_threepfs);
+    z425.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("425", "425");
+    cost425.set_label_text("425", "425");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z450(ztk450, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost450(*dynamic_cast< transport::threepf_task<>* >(ztk450.get_parent_task()), all_threepfs);
+    z450.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("450", "450");
+    cost450.set_label_text("450", "450");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z475(ztk475, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost475(*dynamic_cast< transport::threepf_task<>* >(ztk475.get_parent_task()), all_threepfs);
+    z475.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("475", "475");
+    cost475.set_label_text("474", "474");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z500(ztk500, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost500(*dynamic_cast< transport::threepf_task<>* >(ztk500.get_parent_task()), all_threepfs);
+    z500.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("500", "500");
+    cost500.set_label_text("500", "500");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z525(ztk525, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost525(*dynamic_cast< transport::threepf_task<>* >(ztk525.get_parent_task()), all_threepfs);
+    z525.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("525", "525");
+    cost525.set_label_text("525", "525");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z550(ztk550, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost550(*dynamic_cast< transport::threepf_task<>* >(ztk550.get_parent_task()), all_threepfs);
+    z550.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("550", "550");
+    cost550.set_label_text("550", "550");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z575(ztk575, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost575(*dynamic_cast< transport::threepf_task<>* >(ztk575.get_parent_task()), all_threepfs);
+    z575.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("575", "575");
+    cost575.set_label_text("575", "575");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z600(ztk600, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost600(*dynamic_cast< transport::threepf_task<>* >(ztk600.get_parent_task()), all_threepfs);
+    z600.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("600", "600");
+    cost600.set_label_text("600", "600");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z625(ztk625, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost625(*dynamic_cast< transport::threepf_task<>* >(ztk625.get_parent_task()), all_threepfs);
+    z625.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("625", "625");
+    cost625.set_label_text("625", "625");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z650(ztk650, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost650(*dynamic_cast< transport::threepf_task<>* >(ztk650.get_parent_task()), all_threepfs);
+    z650.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("650", "650");
+    cost650.set_label_text("650", "650");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z675(ztk675, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost675(*dynamic_cast< transport::threepf_task<>* >(ztk675.get_parent_task()), all_threepfs);
+    z675.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("675", "675");
+    cost675.set_label_text("675", "675");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z700(ztk700, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost700(*dynamic_cast< transport::threepf_task<>* >(ztk700.get_parent_task()), all_threepfs);
+    z700.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("700", "700");
+    cost700.set_label_text("700", "700");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z725(ztk725, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost725(*dynamic_cast< transport::threepf_task<>* >(ztk725.get_parent_task()), all_threepfs);
+    z725.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("725", "725");
+    cost725.set_label_text("725", "725");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z750(ztk750, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost750(*dynamic_cast< transport::threepf_task<>* >(ztk750.get_parent_task()), all_threepfs);
+    z750.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("750", "750");
+    cost750.set_label_text("750", "750");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z775(ztk775, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost775(*dynamic_cast< transport::threepf_task<>* >(ztk775.get_parent_task()), all_threepfs);
+    z775.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("775", "775");
+    cost775.set_label_text("775", "775");
+
+    vis_toolkit::zeta_threepf_wavenumber_series<> z800(ztk800, last_time, all_threepfs);
+    vis_toolkit::cost_wavenumber<> cost800(*dynamic_cast< transport::threepf_task<>* >(ztk800.get_parent_task()), all_threepfs);
+    z800.set_use_kt_label(true).set_use_alpha_label(true).set_use_beta_label(true).set_label_text("800", "800");
+    cost800.set_label_text("800", "800");
+
+    vis_toolkit::wavenumber_series_table<> convergence_table("convergence_table", "convergence_table.csv");
+    convergence_table += z300 + z325 + z350 + z375
       + z400 + z425 + z450 + z475
       + z500 + z525 + z550 + z575
       + z600 + z625 + z650 + z675
       + z700 + z725 + z750 + z775
-      + z800 + z825 + z850 + z875
-      + z900 + z925 + z950 + z975
-      + z1000;
+      + z800;
 
-    transport::output_task<> out_tk("convergence.output", table);
+    vis_toolkit::wavenumber_series_table<> timing_table("timing_table", "timing_table.csv");
+    timing_table += cost300 + cost325 + cost350 + cost375
+      + cost400 + cost425 + cost450 + cost475
+      + cost500 + cost525 + cost550 + cost575
+      + cost600 + cost625 + cost650 + cost675
+      + cost700 + cost725 + cost750 + cost775
+      + cost800;
+
+    transport::output_task<> out_tk("convergence.output");
+    out_tk += convergence_table + timing_table;
 
     repo.commit(out_tk);
   }
